26b188974c5ca3dbeb3849b14e0a45a667a9fc25
V Guardado <auricia.guardado@gmail.com>
2021-01-14 22:17:21 -0800

    notes html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,19 @@
+//DEPENDENCIES 
+
+var path = require("path");
+
+//ROUTING
+
+module.exports =  function(app) {
+
+//HTML GET requests
 //GET /notes returns the notes.html file 
+
+app.get("/notes", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
 
 //GET * returns index.html
 
+}
