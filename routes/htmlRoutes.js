0213a04de57b6eb389e4bfa20cc03e4b461675bf
V Guardado <auricia.guardado@gmail.com>
2021-01-14 22:21:41 -0800

    route index html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,13 +7,13 @@ var path = require("path");
 module.exports =  function(app) {
 
 //HTML GET requests
-//GET /notes returns the notes.html file 
 
 app.get("/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+app.get("/*", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-//GET * returns index.html
-
-}
+};
